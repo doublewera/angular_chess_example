4db48fcab512fed33831b40417a4be5adf32e81a
top-college-odilbek <odarziev@mail.ru>
2024-07-05 18:43:18 +0300

    Новый синтаксис директивы for

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TableauComponent } from './tableau/tableau.component';
-import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    TableauComponent,
-    NgFor
+    TableauComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
