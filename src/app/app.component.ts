025009924547d4953b363deca722e02569c0c984
top-college-odilbek <odarziev@mail.ru>
2024-07-05 18:37:07 +0300

    Теперь двое "часов" добавляются в цикле (старое)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TableauComponent } from './tableau/tableau.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    TableauComponent
+    TableauComponent,
+    NgFor
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'chessclock';
+  tableaux = [
+    1, 2
+  ];
 }
